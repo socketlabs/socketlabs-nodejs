c35f98047185da7b722e177da30144261ea55f4c
david-schrenker <david.schrenker@socketlabs.com>
2023-01-11 09:18:37 -0500

    adding to post and json serialization

--- src/core/injectionRequestFactory.js
+++ src/core/injectionRequestFactory.js
@@ -1,4 +1,4 @@
-const { ToBulkRecipient, ToEmailAddress, ToCustomHeader, ToMergeData } = require('../helpers/helpersClasses');
+const { ToBulkRecipient, ToEmailAddress, ToCustomHeader, ToMergeData, ToMetadata } = require('../helpers/helpersClasses');
 const { BasicMessage, BulkMessage } = require('../message/messageClasses');
 const sendValidator = require('./sendValidator');
 const sendResultEnum = require('../sendResultEnum');
@@ -8,111 +8,130 @@ class InjectionRequestFactory {
     //constructor(){}
     generateRequest(messageData) {
         return new Promise((resolve, reject) => {
-                var {
-                    to,
-                    from,
-                    replyTo,
-                    subject,
-                    textBody,
-                    htmlBody,
-                    apiTemplate,
-                    cc,
-                    bcc,
-                    attachments,
-                    messageId,
-                    mailingId,
-                    charSet,
-                    customHeaders,
-                    globalMergeData,
-                    messageType
-                } = messageData;
+            var {
+                to,
+                from,
+                replyTo,
+                subject,
+                textBody,
+                htmlBody,
+                apiTemplate,
+                cc,
+                bcc,
+                attachments,
+                messageId,
+                mailingId,
+                charSet,
+                customHeaders,
+                metadata,
+                tags,
+                globalMergeData,
+                messageType
+            } = messageData;
 
-                var result = {};
-                var validator = new sendValidator();
+            var result = {};
+            var validator = new sendValidator();
 
-                if (messageType === "basic") {
+            if (messageType === "basic") {
 
-                    var basicMsg = new BasicMessage();
+                var basicMsg = new BasicMessage();
 
-                    if (to) {
-                        if (Array.isArray(to)) basicMsg.to = to;
-                        else basicMsg.to.push(ToEmailAddress.convert(to));
-                    }
-                    basicMsg.from = from;
-                    basicMsg.replyTo = replyTo;
-                    basicMsg.subject = subject;
-                    basicMsg.textBody = textBody;
-                    basicMsg.htmlBody = htmlBody;
-                    if (attachments) {
-                        if (Array.isArray(attachments)) basicMsg.attachments = attachments;
-                        else basicMsg.attachments.push(attachments);
-                    }
-                    if (cc) {
-                        if (Array.isArray(cc)) basicMsg.cc = cc;
-                        else basicMsg.cc.push(ToEmailAddress.convert(cc));
-                    }
-                    if (bcc) {
-                        if (Array.isArray(bcc)) basicMsg.bcc = bcc;
-                        else basicMsg.bcc.push(ToEmailAddress.convert(bcc));
-                    }
-                    basicMsg.apiTemplate = apiTemplate;
-                    basicMsg.messageId = messageId;
-                    basicMsg.mailingId = mailingId;
-                    basicMsg.charSet = charSet;
-                    if (customHeaders) {
-                        if (Array.isArray(customHeaders)) basicMsg.customHeaders = customHeaders;
-                        else basicMsg.customHeaders.push(ToEmailAddress.convert(customHeaders));
-                    }
+                if (to) {
+                    if (Array.isArray(to)) basicMsg.to = to;
+                    else basicMsg.to.push(ToEmailAddress.convert(to));
+                }
+                basicMsg.from = from;
+                basicMsg.replyTo = replyTo;
+                basicMsg.subject = subject;
+                basicMsg.textBody = textBody;
+                basicMsg.htmlBody = htmlBody;
+                if (attachments) {
+                    if (Array.isArray(attachments)) basicMsg.attachments = attachments;
+                    else basicMsg.attachments.push(attachments);
+                }
+                if (cc) {
+                    if (Array.isArray(cc)) basicMsg.cc = cc;
+                    else basicMsg.cc.push(ToEmailAddress.convert(cc));
+                }
+                if (bcc) {
+                    if (Array.isArray(bcc)) basicMsg.bcc = bcc;
+                    else basicMsg.bcc.push(ToEmailAddress.convert(bcc));
+                }
+                basicMsg.apiTemplate = apiTemplate;
+                basicMsg.messageId = messageId;
+                basicMsg.mailingId = mailingId;
+                basicMsg.charSet = charSet;
+                if (customHeaders) {
+                    if (Array.isArray(customHeaders)) basicMsg.customHeaders = customHeaders;
+                    else basicMsg.customHeaders.push(ToEmailAddress.convert(customHeaders));
+                }
+                if (metadata) {
+                    if (Array.isArray(metadata)) basicMsg.metadata = metadata;
+                    else basicMsg.metadata.push(ToMetadata.convert(metadata));
+                }
+                if (tags) {
+                    if (Array.isArray(tags)) basicMsg.tags = tags;
+                    else basicMsg.tags.push(tags);
+                }
 
-                    result = validator.validateBasicMessage(basicMsg);
-                    if (result.result !== sendResultEnum.Success) {
-                        reject(result);
-                    }
-
-                    resolve(basicMsg.toJSON());
-                } else if (messageType === "bulk") {
-                    var bulkMsg = new BulkMessage();
-
-                    if (to) {
-                        if (Array.isArray(to)) bulkMsg.to = to;
-                        else bulkMsg.to.push(ToBulkRecipient.convert(to));
-                    }
-                    bulkMsg.from = from;
-                    bulkMsg.replyTo = replyTo;
-                    bulkMsg.subject = subject;
-                    bulkMsg.textBody = textBody;
-                    bulkMsg.htmlBody = htmlBody;
-                    if (attachments) {
-                        if (Array.isArray(attachments)) bulkMsg.attachments = attachments;
-                        else bulkMsg.attachments.push(attachments);
-                    }
-                    bulkMsg.apiTemplate = apiTemplate;
-                    bulkMsg.messageId = messageId;
-                    bulkMsg.mailingId = mailingId;
-                    bulkMsg.charSet = charSet;
-                    if (customHeaders) {
-                        if (Array.isArray(customHeaders)) bulkMsg.customHeaders = customHeaders;
-                        else bulkMsg.customHeaders.push(ToCustomHeader.convert(customHeaders));
-                    }
-                    if (globalMergeData) {
-                        if (Array.isArray(globalMergeData)) bulkMsg.globalMergeData = globalMergeData;
-                        else bulkMsg.globalMergeData.push(ToMergeData.convert(globalMergeData));
-                    }
-
-                    result = validator.validateBulkMessage(bulkMsg);
-                    if (result.result !== sendResultEnum.Success) {
-                        reject(result);
-                    }
-
-                    resolve(bulkMsg.toJSON());
-                } else {
-                    result = new sendResponse({
-                        result: sendResultEnum.MessageValidationInvalidMessageType
-                    });
+                result = validator.validateBasicMessage(basicMsg);
+                if (result.result !== sendResultEnum.Success) {
                     reject(result);
                 }
-            }
-        )}
-    }
 
-    module.exports = new InjectionRequestFactory();
+                resolve(basicMsg.toJSON());
+            } else if (messageType === "bulk") {
+                var bulkMsg = new BulkMessage();
+
+                if (to) {
+                    if (Array.isArray(to)) bulkMsg.to = to;
+                    else bulkMsg.to.push(ToBulkRecipient.convert(to));
+                }
+                bulkMsg.from = from;
+                bulkMsg.replyTo = replyTo;
+                bulkMsg.subject = subject;
+                bulkMsg.textBody = textBody;
+                bulkMsg.htmlBody = htmlBody;
+                if (attachments) {
+                    if (Array.isArray(attachments)) bulkMsg.attachments = attachments;
+                    else bulkMsg.attachments.push(attachments);
+                }
+                bulkMsg.apiTemplate = apiTemplate;
+                bulkMsg.messageId = messageId;
+                bulkMsg.mailingId = mailingId;
+                bulkMsg.charSet = charSet;
+                if (customHeaders) {
+                    if (Array.isArray(customHeaders)) bulkMsg.customHeaders = customHeaders;
+                    else bulkMsg.customHeaders.push(ToCustomHeader.convert(customHeaders));
+                }
+                if (metadata) {
+                    if (Array.isArray(metadata)) bulkMsg.metadata = metadata;
+                    else bulkMsg.metadata.push(ToMetadata.convert(metadata));
+                }
+                if (tags) {
+                    if (Array.isArray(tags)) bulkMsg.tags = tags;
+                    else bulkMsg.tags.push(tags);
+                }
+                if (globalMergeData) {
+                    if (Array.isArray(globalMergeData)) bulkMsg.globalMergeData = globalMergeData;
+                    else bulkMsg.globalMergeData.push(ToMergeData.convert(globalMergeData));
+                }
+
+                result = validator.validateBulkMessage(bulkMsg);
+                if (result.result !== sendResultEnum.Success) {
+                    reject(result);
+                }
+
+                resolve(bulkMsg.toJSON());
+            } else {
+                result = new sendResponse({
+                    result: sendResultEnum.MessageValidationInvalidMessageType
+                });
+                reject(result);
+            }
+        }
+        )
+    }
+}
+
+module.exports = new InjectionRequestFactory();
